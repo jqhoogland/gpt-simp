3df47ec252ff674a2986a742c9d9de426f500cbd
Jesse Hoogland <jessequinten@gmail.com>
2023-02-18 11:07:59 -0800

    ADD working retrieval of wikitext for page

--- gpt_simp/wiki.py
+++ gpt_simp/wiki.py
@@ -1,10 +1,39 @@
+import json
+from typing import Any, Dict, Optional
+
+import requests
 import wikipediaapi
 
 
+def get_session() -> requests.Session:
+    default_headers = dict()
+    default_headers.setdefault(
+        "User-Agent",
+        "Wikipedia-API (https://github.com/martin-majlis/Wikipedia-API)",
+    )
+    session = requests.Session()
+    session.headers.update(default_headers)
+    return session
+
+
+def fetch_wiki_article(name: str, params: Optional[Dict[str, Any]]=None, language="en", ) -> str:
+    if params is None:
+        params = dict()
+
+    base_url = "https://" + language + ".wikipedia.org/w/api.php"
+    params["format"] = "json"
+    params["redirects"] = 1
+    params["action"] = "query"
+    params["prop"] =  "revisions"
+    params["rvprop"] = "content"
+    params["titles"] = name
+    r = get_session().get(base_url, params=params, timeout=10)
+    return r.json()
+
+
 def get_wiki_article(name: str) -> str:
-    wiki_wiki = wikipediaapi.Wikipedia('en')
-    page_py = wiki_wiki.page(name)
-    return page_py.text
+    article = fetch_wiki_article(name)
+    return list(article["query"]["pages"].values())[0]["revisions"][0]["*"]
 
 
 def publish(name: str, article: str) -> None:
